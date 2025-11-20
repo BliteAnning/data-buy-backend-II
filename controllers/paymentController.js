18b74c7ea2cf6a15e5ce41ca65dcc7e80268bd73
Bright Anning <anningbright578@gmail.com>
2025-11-20 21:48:07 -0100

    testing bulkclist

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -3,9 +3,9 @@ import { db } from '../Config/firebase.js';
 
 
 
-
+console.log("test")
 export const sendMobileMoney = async (req, res) => {
-    const {account_number, account_name, channel, client_reference, amount } = req.body
+    const { account_number, account_name, channel, client_reference, amount } = req.body
 
     try {
 
@@ -29,35 +29,52 @@ export const sendMobileMoney = async (req, res) => {
                 account_name: account_name,
                 client_reference: client_reference
             },
-            /*{
-                authorization:{
-                    key: x-api-key,
-                    value: process.env.BULKCLIX_API_KEY
-                }
-            },*/
+           
             
             {
                 headers: {
-                    Accept: "application/json",
-                    "x-api-key": process.env.BULKCLIX_API_KEY
+                    "Accept": "application/json",
+                    "x-api-key": process.env.BULKCLIX_API_KEY,
+                    "Content-Type": "application/json"
                    
                 }
             }
         );
 
+       
+     
         console.log("Response:", response.data);
-        res.json({ 
-            success: true, 
+        res.json({
+            success: true,
             data: response.data,
-            message: "withdrawal initiated" 
+            message: "withdrawal initiated"
         });
+        console.log(process.env.BULKCLIX_API_KEY);
 
     } catch (error) {
         console.error("Error:", error.response?.data || error.message);
-        res.json({ 
-            error: true, 
-            message: error.response?.data || error.message, 
-            success:false
+        res.json({
+            error: true,
+            message: error.response?.data || error.message,
+            success: false
         });
     }
 }
+
+
+
+
+
+
+
+/*curl -X POST "https://api.bulkclix.com/api/v1/payment-api/send/mobilemoney" \
+  -H "Accept: application/json" \
+  -H "Content-Type: application/json" \
+  -H "x-api-key: ACdJEyLckKSJ7GtIef4n0cBj3sRawbeRQg5ccaDg" \
+  -d '{
+        "amount": "1",
+        "account_number": "0551956879",
+        "channel": "MTN",
+        "account_name": "Bright",
+        "client_reference": "8493849839448878849384"
+      }'*/
--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use("/api", paymentRoute)
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 
     app.listen(PORT, "0.0.0.0", () => {
