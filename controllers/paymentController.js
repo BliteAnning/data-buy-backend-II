2da7402ecf68761f9c5c54151a5701a0175dd6aa
Bright Anning <anningbright578@gmail.com>
2025-11-21 18:15:59 -0100

    testing bulkclist with db

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -14,7 +14,7 @@ export const sendMobileMoney = async (req, res) => {
     .get();
     
     
-    if (!subSnap.empty) {
+    if (subSnap.empty) {
       return res.status(404).json({ error: "uid not found" });
     }
 
